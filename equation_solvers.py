ff2175864b06e046c0a1e9794b8339b19267a05e
mswiniar <marcin.swiniarski@intel.com>
2020-01-17 20:07:44 +0100

    init commit

---
+++ equation_solvers.py
@@ -0,0 +1,254 @@
+import numpy as np
+from timeit import default_timer as timer
+import scipy.sparse.linalg
+
+
+def partial_pivot_gauss(a, b, eps=1e-6):
+    start_partial_pivoting = timer()
+    n = len(b)
+    A = np.copy(a)
+    b = np.copy(b)
+    X = np.zeros(n)
+    for k in range(0, n-1):
+        if np.abs(A[k, k]) < eps:
+            for i in range(k+1, n):
+                if np.abs(A[i, k]) > np.abs(A[k, k]):
+                    A[[k, i]] = A[[i, k]]
+                    b[[k, i]] = b[[i, k]]
+                    break
+        # Gauss elimination
+        for i in range(k+1, n):
+            if A[k, k] == 0:
+                continue
+            coeff = A[i, k] / A[k, k]
+            for j in range(k, n):
+                A[i, j] = A[i, j] - coeff*A[k, j]
+            b[i] = b[i] - coeff*b[k]
+    
+    # Reverse Gauss procedure
+    X[n-1] = b[n-1]/A[n-1, n-1]
+    for i in reversed(range(0, n-1)):
+       sum_ax = 0
+       for j in range(i+1, n):
+           sum_ax += A[i, j] * X[j]
+       X[i] = (b[i] - sum_ax)/A[i, i]
+    
+    stop_partial_pivoting = timer()
+    print(f"(PG) Partial pivoting gauss equation solve time: {stop_partial_pivoting - start_partial_pivoting}")
+    
+    return X
+
+
+def gauss_seidel_update_x(A, x, b):
+    n = len(x)
+    for i in range(n):
+        temp = b[i]
+        for j in range(n):
+            if i != j:
+                temp -= A[i][j] * x[j]
+        x[i] = temp / A[i][i]
+        
+    return x
+
+
+def gauss_seidel_update_x_sparse(compact_matrix, x, b):
+    n = len(x)
+    for i in range(n):
+        temp = b[i]
+        for j in range(compact_matrix.shape[1]):
+            row_index = int(compact_matrix[0, j])
+            column_index = int(compact_matrix[1, j])
+            value = compact_matrix[2, j]
+            if row_index == i:
+                if row_index == column_index:
+                    diag_el = value
+                else:
+                    temp -= value * x[column_index]
+        
+        x[i] = temp / diag_el
+        
+    return x
+
+
+def gauss_seidel(A, b, x=None, stop_criterion=1e-6, nb_iteration=20, check_criterion=False):
+    test_convergence_gauss_seidel(A)
+    start_gauss_seidel = timer()
+    if x is None:
+        x = np.zeros(len(b), dtype=np.float32)
+
+    for i in range(nb_iteration):
+        x_old = np.copy(x) 
+        gauss_seidel_update_x(A, x, b)
+        if check_criterion:
+            if x.all() != 0:
+                update_score = np.abs((x - x_old)/x)
+                if (update_score <= stop_criterion).all():
+                    break
+        
+    stop_gauss_seidel = timer()
+    print(f"(GS) Gauss-Seidel number of iteration: {i+1} to converge in {stop_criterion} update, time consumption: {stop_gauss_seidel - start_gauss_seidel}")
+    
+    return x
+
+
+def gauss_seidel_effective(A, b, x=None, n=20):
+    if x is None:
+        x = np.zeros(len(b), dtype=np.float32)
+    start_gs = timer()
+    L = np.tril(A)
+    U = A - L
+    for i in range(n):
+        x = np.dot(np.linalg.inv(L), b - np.dot(U, x))
+
+    stop_gs = timer()
+    print(f"GS: Time consumption: {stop_gs - start_gs}")
+    return x
+
+
+def gauss_seidel_sparse(A_sparse, b, x=None, nb_iteration=20, stop_criterion=1e-6, check_criterion=False):
+    test_convergence_gauss_seidel(A_sparse)
+    compact_matrix = get_compact_matrix(A_sparse)
+    start_gauss_seidel = timer()
+    if x is None:
+        x = np.zeros(len(b), dtype=np.float32)
+        
+    for i in range(nb_iteration):
+        x_old = np.copy(x) 
+        gauss_seidel_update_x_sparse(compact_matrix, x, b)
+        if check_criterion:
+            if x.all() != 0:
+                update_score = np.abs((x - x_old)/x)
+                if (update_score <= stop_criterion).all():
+                    break
+        
+    stop_gauss_seidel = timer()
+    print(f"(IS) Gauss-Seidel Sparse: number of iteration: {i+1} to converge in {stop_criterion} update, time consumption: {stop_gauss_seidel - start_gauss_seidel}")
+    
+    return x
+
+
+def jacobi(A, b, nb_iter=25, x=None):
+    """Solves the equation Ax=b via the Jacobi iterative method."""
+    start_jacobi = timer()
+    # Create an initial guess if needed                                                                                                                                                            
+    if x is None:
+        x = np.zeros(len(b), dtype=np.float32)
+
+    # Create a vector of the diagonal elements of A                                                                                                                                                
+    # and subtract them from A                                                                                                                                                                     
+    D = np.diag(A)
+    R = A - np.diagflat(D)
+    # Iterate for N times                                                                                                                                                                          
+    for i in range(nb_iter):
+        x = (b - np.dot(R,x)) / D
+
+    stop_jacobi = timer()
+    print(f"(IT) Jacobi: number of iteration {i+1} time consumption: {stop_jacobi-start_jacobi}")
+    return x
+
+
+def get_compact_matrix(sparse_matrix):
+    non_zero_elements_idxs = np.argwhere(sparse_matrix != 0)
+    row_indexes = non_zero_elements_idxs[:, 0]
+    column_indexes = non_zero_elements_idxs[:, 1]
+    nb_elements = len(non_zero_elements_idxs)
+    
+    compact_matrix = np.zeros((3, nb_elements), dtype=np.float32)
+    compact_matrix[0, :] = row_indexes
+    compact_matrix[1, :] = column_indexes
+    compact_matrix[2, :] = sparse_matrix[row_indexes, column_indexes]
+    
+    return compact_matrix
+
+
+def sparse_LU(sparse_matrix, b):
+    start_LU = timer()
+    A = scipy.sparse.csc_matrix(sparse_matrix)
+    LU = scipy.sparse.linalg.splu(A)
+    x = LU.solve(b)
+    stop_LU = timer()
+    print(f"Sparse LU scipy solver time consumption: {stop_LU - start_LU}")
+    return x
+
+
+def sparse_iterative(sparse_matrix, b, criterion=1e-5, max_iter=None):
+    start_iterative = timer()
+    x = scipy.sparse.linalg.bicg(sparse_matrix, b, tol=criterion, maxiter=max_iter)[0]
+    stop_iterative = timer()
+    print(f"Sparse iterative method using scipy solver time consumption: {stop_iterative - start_iterative}")
+    return x
+
+
+def test_convergence_gauss_seidel(A):
+    converges = True
+    for i in range(A.shape[0]):
+        for j in range(A.shape[0]):
+            sum_j = 0
+            if i != j:
+                sum_j += np.abs(A[i, j])
+                if np.abs(A[i, i]) < sum_j:
+                    converges = False        
+    if converges:
+        print("Gauss-seidel: The convergence test has been passed")
+    else:
+        print("\nGauss-seidel iteration method to solve linear equation is not converging!\n")
+                
+    
+def test_partial_pivoting(a, b, numpy_solution):        
+    solution = partial_pivot_gauss(a, b)
+    print(f"Partial pivot solution : {solution}")
+    avg_err = np.mean(np.abs(numpy_solution - solution))
+    print(f"Average error between numpy and partial pivoting: {avg_err}")
+
+
+def test_gauss_seidel(a, b, numpy_solution, stop_criterion=1e-6, nb_iter=20):
+    solution = gauss_seidel(a, b, stop_criterion=stop_criterion, nb_iteration=nb_iter)
+    print(f"Gauss-Seidel solution : {solution}")
+    avg_err = np.mean(np.abs(numpy_solution - solution))
+    print(f"Average error between numpy and Gauss-Seidel: {avg_err}")
+
+
+def test_compact_matrix(sparse_matrix):
+    compact_matrix = get_compact_matrix(sparse_matrix)
+    print("Sparse matrix")
+    print(sparse_matrix)
+    print("Compact matrix")
+    print(compact_matrix)
+
+
+def test_sparse_GS(a, b, numpy_solution, stop_criterion=1e-6, nb_iter=20):
+    solution = gauss_seidel_sparse(a, b, stop_criterion=stop_criterion, nb_iteration=nb_iter)
+    print(f"Sparse Gauss-Seidel solution : {solution}")
+    avg_err = np.mean(np.abs(numpy_solution - solution))
+    print(f"Average error between numpy and Sparse Gauss-Seidel: {avg_err}")
+
+
+def test_sparse_scipy(a, b, numpy_solution):
+    x_lu = sparse_LU(a, b)
+    avg_err = np.mean(np.abs(numpy_solution - x_lu))
+    print(f"Average error between numpy and Sparse LU: {avg_err}")
+    x_iterative = sparse_iterative(a, b)
+    avg_err = np.mean(np.abs(numpy_solution - x_iterative))
+    print(f"Average error between numpy and iterative sparse scipy: {avg_err}")
+
+    
+if __name__ == "__main__":
+    # a = np.array([[0, 31, -1, 3, 1, -15],
+    #             [0, 32, 0, -11, 70, -3],
+    #             [61, 2, 22, -12, -1, 22],
+    #             [-2, 17, 24, 0, 2, -6],
+    #             [3, 0, 14, -27, 1, -5],
+    #             [62, 31, -4, 5, 2, 0]], dtype=np.float32)
+    # b = np.array([55, 47, 22, 3, 4, 8], dtype=np.float32)
+    
+    a = np.array([[4, 1, 2],[3, 5, 1],[1, 1, 3]], dtype=np.float32) 
+    b = np.array([4,7,3], dtype=np.float32) 
+    # a_sparse = np.array([[0, 1, 0],[3, 2, 0],[0, 3, 2]], dtype=np.float32) 
+    # a = a_sparse
+    # test_compact_matrix(a_sparse)
+    numpy_solution = np.linalg.solve(a, b)
+    print(f"Numpy linalg solution: {numpy_solution}")
+    test_partial_pivoting(a, b, numpy_solution)
+    test_gauss_seidel(a, b, numpy_solution, nb_iter=25)
+    test_sparse_GS(a, b, numpy_solution, nb_iter=25, stop_criterion=1e-6)
+    test_sparse_scipy(a, b, numpy_solution)
---
+++ lab4.py
@@ -0,0 +1,133 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import geopy.distance
+from equation_solvers import partial_pivot_gauss, gauss_seidel, jacobi, gauss_seidel_sparse, sparse_iterative, sparse_LU
+from equation_solvers import gauss_seidel_effective
+
+
+def extract_data_from_txt(file_path, nb_points=-1):
+    data = pd.read_csv(file_path, delimiter=",").values
+    x1, x2, y = data[:nb_points, 1], data[:nb_points, 0], data[:nb_points-1, 2]
+    n = len(x1)
+    x = np.zeros(n-1)
+    for i in range(n-1):
+        x[i] = geopy.distance.distance((x1[i], x2[i]), (x1[i+1], x2[i+1])).km
+        x[i] += x[i-1]
+
+    return x, y
+
+
+def spline_interpolation(x, y, mode="numpy"):
+    if mode == "numpy":
+        linear_solve_eq = np.linalg.solve
+    elif mode == "PG":
+        linear_solve_eq = partial_pivot_gauss
+    elif mode == "IT":
+        linear_solve_eq = jacobi
+    elif mode == "GS":
+        linear_solve_eq = gauss_seidel
+    elif mode == "IS":
+        linear_solve_eq = gauss_seidel_sparse
+    elif mode == "ScipyLU":
+        linear_solve_eq = sparse_LU
+    elif mode == "ScipyIterative":
+        linear_solve_eq = sparse_iterative
+
+    n = len(x)
+    M = np.zeros(len(x), np.float64)
+    A = np.zeros((len(x), len(x)), dtype=np.float64)
+    A[0, 0] = A[-1, -1] = 1
+    for i in range(0, n-2):
+        hi = x[i+1] - x[i]
+        hi_2 = x[i+2] - x[i+1]
+        A[i+1][i:i+3] = [hi, 2*(hi+hi_2), hi_2]
+
+    for i in range(1, n-1):
+        M[i] = 3 * (y[i + 1] - y[i]) / (x[i + 1] - x[i]) - 3 * (y[i] - y[i - 1]) / (x[i] - x[i - 1])
+    M = np.transpose(M)
+    a = np.zeros(n-1)
+    b = np.zeros(n-1)
+    c = linear_solve_eq(A, M)
+    d = np.zeros(n-1)
+
+    for i in range(n-1):
+        a[i] = y[i]
+        b[i] = ((y[i + 1] - y[i]) / (x[i + 1] - x[i])) - ((x[i + 1] - x[i]) / 3) * (2 * c[i] + c[i + 1])
+        d[i] = (c[i + 1] - c[i]) / (3 * (x[i + 1] - x[i]))
+
+    x_ = []
+    y_ = []
+    for i in range(n-1):
+        for p in np.linspace(x[i], x[i + 1], 100):
+            x_.append(p)
+            y_.append(a[i] + b[i] * (p - x[i]) + c[i] * (p - x[i]) ** 2 + d[i] * (p - x[i]) ** 3)
+
+    return x_, y_
+
+
+def extract_test_data(x, y, take_every_nth=3):
+    x_test = x[::take_every_nth]
+    y_test = y[::take_every_nth]
+    x_interpolate = [xi for i, xi in enumerate(x) if i % 3 != 0]
+    y_interpolate = [yi for i, yi in enumerate(y) if i % 3 != 0]
+
+    return np.array(x_interpolate), np.array(y_interpolate), x_test, y_test
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+
+def calc_interpolation_err(x, y, x_test, y_test):
+    err = np.zeros(len(x_test))
+    for i in range(len(x_test)):
+        idx = find_nearest(x, x_test[i])
+        err[i] = np.abs(y_test[i] - y[idx])
+
+    avg_err = np.mean(err)
+    print(f"Average error of interpolation: {avg_err}")
+    return avg_err
+
+
+def main():
+    data_path = "data/trasa2.txt"
+    nb_points = -1
+    x, y = extract_data_from_txt(data_path, nb_points)
+    x, y, x_test, y_test = extract_test_data(x, y)
+
+    x_spline_pg, y_spline_pg = spline_interpolation(x, y, mode="PG")
+    calc_interpolation_err(x_spline_pg, y_spline_pg, x_test, y_test)
+
+    x_spline_gs, y_spline_gs = spline_interpolation(x, y, mode="GS")
+    calc_interpolation_err(x_spline_gs, y_spline_gs, x_test, y_test)
+
+    x_spline_it, y_spline_it = spline_interpolation(x, y, mode="IT")
+    calc_interpolation_err(x_spline_it, y_spline_it, x_test, y_test)
+
+    x_spline_is, y_spline_is = spline_interpolation(x, y, mode="IS")
+    calc_interpolation_err(x_spline_is, y_spline_is, x_test, y_test)
+
+    x_spline_lu, y_spline_lu = spline_interpolation(x, y, mode="ScipyLU")
+    calc_interpolation_err(x_spline_lu, y_spline_lu, x_test, y_test)
+
+    x_spline_sparse_it, y_spline_sparse_it = spline_interpolation(x, y, mode="ScipyIterative")
+    calc_interpolation_err(x_spline_sparse_it, y_spline_sparse_it, x_test, y_test)
+
+    plt.plot(x, y, 'o', label='Real values')
+    plt.plot(x_test, y_test, 'x', label="Test values")
+    plt.plot(x_spline_pg, y_spline_pg, label='PG')
+    plt.plot(x_spline_gs, y_spline_gs, label='GS')
+    plt.plot(x_spline_it, y_spline_it, label='IT')
+    plt.plot(x_spline_is, y_spline_is, label='IS')
+    plt.plot(x_spline_lu, y_spline_lu, label="Sparse LU")
+    plt.plot(x_spline_sparse_it, y_spline_sparse_it, label="Sparse Iterative")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
